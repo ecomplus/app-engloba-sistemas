de198a9fa60442c582a2675dfd79e2f857298986
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-05-24 23:50:23 -0300

    feat: add calculation

--- functions/routes/ecom/modules/calculate-shipping.js
+++ functions/routes/ecom/modules/calculate-shipping.js
@@ -1,3 +1,6 @@
+const axios = require('axios')
+const ecomUtils = require('@ecomplus/utils')
+
 exports.post = ({ appSdk }, req, res) => {
   /**
    * Treat `params` and (optionally) `application` from request body to properly mount the `response`.
@@ -17,12 +20,30 @@ exports.post = ({ appSdk }, req, res) => {
   const response = {
     shipping_services: []
   }
+
   // merge all app options configured by merchant
   const appData = Object.assign({}, application.data, application.hidden_data)
+
+  // get token
+  const { token } = appData
+  if (!token) {
+    // must have configured a3 tecnologia token token
+    return res.status(409).send({
+      error: 'CALCULATE_AUTH_ERR',
+      message: 'Token or document unset on app hidden data (merchant must configure the app)'
+    })
+  }
 
   if (appData.free_shipping_from_value >= 0) {
     response.free_shipping_from_value = appData.free_shipping_from_value
   }
+
+  const destinationZip = params.to ? params.to.zip.replace(/\D/g, '') : ''
+  const originZip = params.from
+    ? params.from.zip.replace(/\D/g, '')
+    : appData.zip ? appData.zip.replace(/\D/g, '') : ''
+
+  
   if (!params.to) {
     // just a free shipping preview with no shipping address received
     // respond only with free shipping option
@@ -32,14 +53,74 @@ exports.post = ({ appSdk }, req, res) => {
 
   /* DO THE STUFF HERE TO FILL RESPONSE OBJECT WITH SHIPPING SERVICES */
 
-  /**
-   * Sample snippets:
+  if (!originZip) {
+    // must have configured origin zip code to continue
+    return res.status(409).send({
+      error: 'CALCULATE_ERR',
+      message: 'Zip code is unset on app hidden data (merchant must configure the app)'
+    })
+  }
+
 
   if (params.items) {
-    let totalWeight = 0
-    params.items.forEach(item => {
-      // treat items to ship
-      totalWeight += item.quantity * item.weight.value
+    let finalWeight = 0
+    params.items.forEach(({ quantity, dimensions, weight }) => {
+      let physicalWeight = 0
+      let cubicWeight = 1
+
+      // sum physical weight
+      if (weight && weight.value) {
+        switch (weight.unit) {
+          case 'kg':
+            physicalWeight = weight.value
+            break
+          case 'g':
+            physicalWeight = weight.value / 1000
+            break
+          case 'mg':
+            physicalWeight = weight.value / 1000000
+        }
+      }
+
+      // sum total items dimensions to calculate cubic weight
+      if (dimensions) {
+        const sumDimensions = {}
+        for (const side in dimensions) {
+          const dimension = dimensions[side]
+          if (dimension && dimension.value) {
+            let dimensionValue
+            switch (dimension.unit) {
+              case 'cm':
+                dimensionValue = dimension.value
+                break
+              case 'm':
+                dimensionValue = dimension.value * 100
+                break
+              case 'mm':
+                dimensionValue = dimension.value / 10
+            }
+            // add/sum current side to final dimensions object
+            if (dimensionValue) {
+              sumDimensions[side] = sumDimensions[side]
+                ? sumDimensions[side] + dimensionValue
+                : dimensionValue
+            }
+          }
+        }
+
+        // calculate cubic weight
+        // https://suporte.boxloja.pro/article/82-correios-calculo-frete
+        // (C x L x A) / 6.000
+        for (const side in sumDimensions) {
+          if (sumDimensions[side]) {
+            cubicWeight *= sumDimensions[side]
+          }
+        }
+        if (cubicWeight > 0) {
+          cubicWeight *= 167
+        }
+      }
+      finalWeight += (quantity * (cubicWeight > 50 ? cubicWeight : physicalWeight))
     })
   }
 
@@ -62,8 +143,6 @@ exports.post = ({ appSdk }, req, res) => {
       }
     }
   })
-
-  */
 
   res.send(response)
 }
